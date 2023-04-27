8f0499649580064ac358bc3f9f37f8186668ef32
JosefStonon <josef.sartori@outlook.com>
2023-04-27 16:08:34 -0300

    estilização

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,3 +1,12 @@
 export default {
-  backgroundColor: '#f6f5fc',
+  colors: {
+    backGround: '#F6F5FC',
+    primary: {
+      lighter: '#E0E3FF',
+      light: '#6674F4',
+      main: '#5061FC',
+      dark: '#3346f0',
+    },
+
+  },
 };
